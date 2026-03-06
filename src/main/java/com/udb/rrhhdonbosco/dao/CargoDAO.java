046830d7a51025bc2342dd2c5fb7162f2253c573
Gerson Borja <hipersocial08@gmail.com>
2026-03-06 01:44:06 -0600

    CRUD cargos: agregar, editar, eliminar y mejoras en vistas

--- src/main/java/com/udb/rrhhdonbosco/dao/CargoDAO.java
+++ src/main/java/com/udb/rrhhdonbosco/dao/CargoDAO.java
@@ -1,4 +1,82 @@
 package com.udb.rrhhdonbosco.dao;
 
+import com.udb.rrhhdonbosco.model.Cargo;
+import com.udb.rrhhdonbosco.util.ConexionDB;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class CargoDAO {
+
+    public int agregarCargo(Cargo cargo) {
+        String sql = "INSERT INTO Cargos (cargo, descripcionCargo, jefatura) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexionDB.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, cargo.getCargoNombre());
+            ps.setString(2, cargo.getCargoDescripcion());
+            ps.setBoolean(3, cargo.getCargoJefatura());
+
+            return ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public int editarCargo(Cargo cargo) {
+    String sql = "UPDATE Cargos SET cargo = ?, descripcionCargo = ?, jefatura = ? WHERE idCargo = ?";
+
+    try (Connection conn = ConexionDB.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+
+        ps.setString(1, cargo.getCargoNombre());
+        ps.setString(2, cargo.getCargoDescripcion());
+        ps.setBoolean(3, cargo.getCargoJefatura());
+        ps.setInt(4, cargo.getIdCargo());
+
+        return ps.executeUpdate();
+
+    } catch (SQLException e) {
+        throw new RuntimeException("Error editando cargo", e);
+    }
+}
+
+    public ArrayList<Cargo> listaDeCargos() {
+        String sql = "SELECT * FROM Cargos ORDER BY idCargo DESC";
+        ArrayList<Cargo> lista = new ArrayList<>();
+
+        try (Connection conn = ConexionDB.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Cargo c = new Cargo();
+                c.setIdCargo(rs.getInt("idCargo"));
+                c.setCargoNombre(rs.getString("cargo"));
+                c.setCargoDescripcion(rs.getString("descripcionCargo"));
+                c.setCargoJefatura(rs.getBoolean("jefatura"));
+
+                lista.add(c);
+            }
+
+            return lista;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error listando cargos", e);
+        }
+    }
+    
+    public int eliminarCargo(int idCargo) {
+    String sql = "DELETE FROM Cargos WHERE idCargo = ?";
+
+    try (Connection conn = ConexionDB.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+
+        ps.setInt(1, idCargo);
+
+        return ps.executeUpdate();
+
+    } catch (SQLException e) {
+        throw new RuntimeException("Error eliminando cargo", e);
+    }
+}
 }
--- src/main/java/com/udb/rrhhdonbosco/model/Cargo.java
+++ src/main/java/com/udb/rrhhdonbosco/model/Cargo.java
@@ -1,4 +1,47 @@
 package com.udb.rrhhdonbosco.model;
 
 public class Cargo {
+    private int idCargo;
+    private String cargoNombre;
+    private String cargoDescripcion;
+    private boolean cargoJefatura;
+    
+    public Cargo(){}
+    
+    public Cargo(String cargoNombre, String cargoDescripcion, boolean cargoJefatura){
+        this.cargoNombre = cargoNombre;
+        this.cargoDescripcion =  cargoDescripcion;
+        this.cargoJefatura = cargoJefatura;
+    }
+    
+    public int getIdCargo(){
+        return idCargo;
+    }
+    public String getCargoNombre(){
+        return cargoNombre;
+    }
+    
+    public String getCargoDescripcion(){
+        return cargoDescripcion;
+    }
+    
+    public boolean getCargoJefatura() {
+        return cargoJefatura;
+    }
+    
+    public void setIdCargo(int idCargo){
+        this.idCargo = idCargo;
+    }
+    
+    public void setCargoNombre(String cargoNombre){
+        this.cargoNombre = cargoNombre;
+    }
+    
+    public void setCargoDescripcion(String cargoDescripcion){
+        this.cargoDescripcion = cargoDescripcion;
+    }
+    
+    public void setCargoJefatura(boolean cargoJefatura){
+        this.cargoJefatura = cargoJefatura;
+    }
 }
